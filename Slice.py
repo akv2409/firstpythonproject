556ea4dfaaaf403a73a9293c66dd67d6333b0b98
akv2409 <akv2409@gmail.com>
2023-05-21 10:44:44 +0100

    Commited

---
+++ Slice.py
@@ -0,0 +1,40 @@
+a = ["abc", "bvs", "dff", "abr", "ccd", "few", "utg", "poi"]
+print(f"first 3 items are {a[0:3]}")
+m = len(a) // 2
+# print(a[m - 1: m + 2])
+# print(a[len(a) - 3: len(a)])
+b = a[0:len(a)]
+a.append('chaman')
+b.append('siman')
+
+shape = {
+    'circle': 'gol',
+    'square': 'chaur',
+    'triangle': 'trikon',
+    'squar': 'chaur'
+}
+for sh in set(shape.values()):
+    print(sh.title())
+
+lang = {'chinese','japanese','tamil','kannad'}
+print(lang)
+for l in lang:
+    print(l.title())
+
+riv = {
+    'Ganga':'India',
+    'Nile':'Egypt',
+    'Thames':'England',
+    'Amazon':'America'
+}
+
+for r in riv.keys():
+    print(f"River {r.title()} runs through {riv.get(r.title())}")
+
+   users = {
+       'aeinstein': 90,
+       'mcurie': {'first': 'marie','last': 'curie','location': 'paris'}
+    }
+
+#'aeinstein': {'first': 'albert', 'last': 'einstein', 'location': 'princeton'},
+#'mcurie': {'first': 'marie','last': 'curie','location': 'paris'}
--- new.py
+++ new.py
@@ -1,3 +1,9 @@
-print('new')
-print('jai mata di')
-print('Bum Bhole')
+macs = ["amit","sumit","Mohit","vinit"]
+for mac in macs:
+    print(f"{mac.title()}, Jaldi Ghar aa")
+print(macs[:])
+a=macs[:]
+a.append('jai')
+macs.append('abhay')
+print(f"\n{a}")
+print(macs)
